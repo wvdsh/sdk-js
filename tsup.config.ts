e516f2ee8a7906919199c3e32caf2f1f5d79c9fd
Calvin Studebaker <calstudebaker@gmail.com>
2025-06-27 16:33:41 -0700

    add browser build command for local testing

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["cjs", "esm", "iife"],
+  format: ["cjs", "esm"],
   // These options help when building in CI/CD where peer deps might not be installed
   dts: {
     resolve: true,
