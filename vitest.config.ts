beb526d975f57a367a583b809115f001958f70b0
Raymond Kennedy <franzwarning@gmail.com>
2026-09-20 12:11:55 -0700

    Remove test scaffolding from SDK volume PR

--- vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-  test: {
-    server: { deps: { inline: ["@wvdsh/api"] } },
-    environment: "happy-dom",
-    include: ["tests/**/*.test.ts"]
-  }
-});
